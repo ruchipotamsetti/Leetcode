18d1b1ee711be4a15b24f14a3775046a125f7ccd
ruchipotamsetti <potamsettiruchita@gmail.com>
2026-06-07 14:03:44 -0400

    1971. Find if Path Exists in Graph

---
+++ Graphs/Find if Path Exists in Graph.cpp
@@ -0,0 +1,37 @@
+// Time Complexity: O(V + E) where V is the number of vertices and E is the number of edges in the graph. 
+// We are traversing through all the vertices and edges once in the worst case.
+// Space Complexity: O(V) where V is the number of vertices in the graph. 
+// We are using an adjacency list to store the graph and a visited set to keep track of the visited vertices.    
+
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        vector<vector<int>>conn(n);
+
+        for(int i=0;i<edges.size();i++){
+            conn[edges[i][0]].push_back(edges[i][1]);
+            conn[edges[i][1]].push_back(edges[i][0]);
+        }
+        unordered_set<int>visited;
+        return dfs(source, destination, conn, visited);
+
+        
+    }
+
+    bool dfs(int node, int destination, vector<vector<int>>& conn, unordered_set<int>& visited){
+        if(node == destination)
+            return true;
+        
+        visited.insert(node);
+
+        for(auto neighbor: conn[node]){
+            if(visited.find(neighbor) == visited.end()){
+                if(dfs(neighbor, destination, conn, visited)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
